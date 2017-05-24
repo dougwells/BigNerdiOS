34c309b19aa27249af50c32b26b5bfa966d4c5de
dougwells <dougwells@gmail.com>
2017-05-24 13:15:20 -0600

    Big Nerd Ranch Book. Chapter 1. Outlets and Actions.

---
+++ BigNerdChapt1/BigNerdChapt1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BigNerdChapt1
+//
+//  Created by Doug Wells on 5/24/17.
+//  Copyright © 2017 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
