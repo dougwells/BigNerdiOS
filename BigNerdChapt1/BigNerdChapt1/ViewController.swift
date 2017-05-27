d993074090a6f80158df26bb8af90b180874b970
dougwells <dougwells@gmail.com>
2017-05-26 23:01:23 -0600

    Big Nerd. Chapter 1. Actions/Buttons, outlets/labels.

--- BigNerdChapt1/BigNerdChapt1/ViewController.swift
+++ BigNerdChapt1/BigNerdChapt1/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     /* Notes:
         1) @IBAction or @IBOutlet says connection will be made in Interface Builder
-        2) Target & Action
+     2) Target & Action: When a UIButton is tapped, it calls a method on another object. That object is called the target. The method that is triggered is called the action.      
     */
     
     @IBOutlet var questionLabel: UILabel!
